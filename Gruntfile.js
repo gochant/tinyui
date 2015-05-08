735e96398cc9de5f3b96adbf77f279a3ebcefba9
channing <guochant@gmail.com>
2015-05-08 11:35:52 +0800

    add dist folder

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Task configuration.
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                globals: {
+                    jQuery: true
+                }
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib_test: {
+                src: ['lib/**/*.js', 'test/**/*.js']
+            }
+        },
+        nodeunit: {
+            files: ['test/**/*_test.js']
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.lib_test.src %>',
+                tasks: ['jshint:lib_test', 'nodeunit']
+            }
+        },
+        less: {
+            production: {
+                options: {
+                    modifyVars: {
+                        'font-url': '"../font/fonttiny"'
+                    }
+                },
+                files: {
+                    "dist/css/tiny.css": "src/index.less"
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                  { expand: true, src: ['src/assets/bootstrap-theme-tiny/bootstrap.css'], dest: 'dist/css/', flatten: true },
+                  { expand: true, src: ['src/assets/bootstrap-theme-tiny/bootstrap.js'], dest: 'dist/js/', flatten: true },
+                  { expand: true, src: ['src/font/*'], dest: 'dist/font/', flatten: true },
+                  { expand: true, src: ['src/js/*'], dest: 'dist/js/', filter: 'isFile', flatten: true }
+                ]
+            },
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task.
+    grunt.registerTask('default', ['less', 'copy']);
+
+};
---
+++ dist/js/jquery.submenu.js
@@ -0,0 +1,98 @@
+﻿; (function ($, window, document, undefined) {
+
+    "use strict";
+
+    var pluginName = "submenu";
+    var defaults = {};
+    var MIN_CLASS = 'sidebar-min';
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.minimized = $(this.element).hasClass(MIN_CLASS);
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.icons = ['fa-angle-double-left', 'fa-angle-double-right'];
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+        init: function () {
+            var $navlist = $(this.element).find('.nav-list');
+            var $toggle = $(this.element).find('.sidebar-toggle');
+            var me = this;
+
+            $navlist.on('click', 'a', function (e) {
+                var $link = $(e.currentTarget);
+                var $li = $link.parent();
+
+                if ($li.children('.submenu').length === 0) {
+                    me.active($li);
+                }
+
+                if ($link.hasClass('dropdown-toggle')) {
+                    if ($li.parent().hasClass('nav-list') && me.minimized || $li.hasClass('hover')) {
+                        return;
+                    }
+                    me.toggleDisplay($li);
+                }
+
+            });
+            $toggle.on('click', function (e) {
+                e.preventDefault();
+                if (me.minimized) {
+                    $(me.element).removeClass(MIN_CLASS);
+                    $toggle.children().removeClass(me.icons[1]).addClass(me.icons[0]);
+                } else {
+                    $(me.element).addClass(MIN_CLASS);
+                    $toggle.children().removeClass(me.icons[0]).addClass(me.icons[1]);
+                    // 隐藏所有submenu
+                    //$(me.element).find('.submenu').hide();
+                }
+                me.minimized = !me.minimized;
+            });
+        },
+        toggleDisplay: function ($li) {
+            var me = this;
+            $li.hasClass('open') ? this._hideSubmenu($li) :
+            this._showSubmenu($li, function () {
+                me._hideSubmenu($li.siblings('.open'));
+            });
+        },
+        _showSubmenu: function ($item, callback) {
+            callback || (callback = function () { });
+            $item.addClass('open')
+            .children('.submenu').slideDown('fast', callback);
+            return this;
+        },
+        _hideSubmenu: function ($item, callback) {
+            callback || (callback = function () { });
+            $item.removeClass('open')
+                        .children('.submenu').slideUp('fast', callback);
+            return this;
+        },
+        active: function (selector) {
+            $(this.element).find('.active').removeClass('active');
+            $(selector).addClass('active')
+                .parentsUntil(this.element, 'li').addClass('active');
+        }
+    });
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+
+            var $this = $(this);
+            var data = $this.data('plugin_' + pluginName);
+
+            if (!data) {
+                $this.data('plugin_' + pluginName, (data = new Plugin(this, options)))
+            }
+
+            if (typeof options == 'string') {
+                data[options]();
+            }
+
+        });
+    };
+
+})(jQuery, window, document);
